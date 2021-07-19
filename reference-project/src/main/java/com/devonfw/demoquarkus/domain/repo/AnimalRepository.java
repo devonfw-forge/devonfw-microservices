c8939bbed8ea875ee8155e087a144dcc2a2650b5
Julian Günther <julian.guenther@capgemini.com>
2021-07-19 11:31:02 +0200

    use named parameters instead of indexed parameter

--- reference-project/src/main/java/com/devonfw/demoquarkus/domain/repo/AnimalRepository.java
+++ reference-project/src/main/java/com/devonfw/demoquarkus/domain/repo/AnimalRepository.java
@@ -3,13 +3,14 @@ package com.devonfw.demoquarkus.domain.repo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.devonfw.demoquarkus.domain.model.Animal;
 
 public interface AnimalRepository extends CrudRepository<Animal, Long>, AnimalFragment {
 	
-	@Query("select a from Animal a where name = ?1")
-	Animal findByName(String name);
+	@Query("select a from Animal a where name = :name")
+	Animal findByName(@Param("name") String name);
 	
 	Page<Animal> findAllByOrderByName();
 }
